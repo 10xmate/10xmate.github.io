42281f9ecfa74ff8f61f830aa973f0f5518ea419
greify <georgi.alexandrov@reify.technology>
2020-06-18 12:26:30 +0300

    add better screen images for our product and rewards pages

--- js/custom-cookie.js
+++ js/custom-cookie.js
@@ -3,8 +3,8 @@ const segments = uri.split("/");
 const cookiePolicyUrl = segments.includes('bg') ? 'https://10xmate.com/bg/privacy-policy' : 'https://10xmate.com/privacy-policy';
 const popupTitle = segments.includes('bg') ? 'Уебсайтът използва бисквитки' : 'This website is using cookies';
 const popupText = segments.includes('bg')
-  ? 'Ние използваме бисквитки, за да анализираме данните по използването на този уебсайт. Ако продължите без да промените вашите настройки, относно бисквитките, ще приемем, че се съгласявате да получите бисквитките от този уебсайт.'
-  : 'We use cookies to analyse the data, received for this website use. If you continue without changing your settings, we\'ll assume that you are happy to receive all cookies on this website.';
+  ? 'Използваме бисквитки, за да анализираме данните от употребата на уебсайта. Можете да продължите да разглеждате, получавайки всички бисквитки, които сайтът използва или да промените своите настройки за бисквитки.'
+  : 'We use cookies to analyse the data received for this website use. If you continue without changing your settings, wе’ll assume that you are happy to receive all cookies on this website.'
 const buttonContinueTitle = segments.includes('bg') ? 'Приемам' : 'Accept';
 const buttonLearnmoreTitle = segments.includes('bg') ? 'Още информация' : 'Learn more';
 
