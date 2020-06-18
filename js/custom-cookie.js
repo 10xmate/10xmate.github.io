0b1af9440bdef3a5c426a3695211ad71347ea60a
greify <georgi.alexandrov@reify.technology>
2020-06-18 21:01:26 +0300

    add terms of use

--- js/custom-cookie.js
+++ js/custom-cookie.js
@@ -3,8 +3,8 @@ const segments = uri.split("/");
 const cookiePolicyUrl = segments.includes('bg') ? 'https://10xmate.com/bg/privacy-policy' : 'https://10xmate.com/privacy-policy';
 const popupTitle = segments.includes('bg') ? 'Уебсайтът използва бисквитки' : 'This website is using cookies';
 const popupText = segments.includes('bg')
-  ? 'Използваме бисквитки, за да анализираме данните от употребата на уебсайта. Можете да продължите да разглеждате, получавайки всички бисквитки, които сайтът използва или да промените своите настройки за бисквитки.'
-  : 'We use cookies to analyse the data received for this website use. If you continue without changing your settings, wе’ll assume that you are happy to receive all cookies on this website.'
+  ? 'Използваме бисквитки, за да анализираме данните от употребата на уебсайта. Можете да продължите да разглеждате, получавайки всички бисквитки, които сайтът използва или да промените своите настройки за бисквитки. С продължаване на разглеждането на уебсайта се съгласявате също с нашите <a target="_blank" href="terms-of-use">Общи условия</a> за ползване.'
+  : 'We use cookies to analyse the data received for this website use. If you continue without changing your settings, wе’ll assume that you are happy to receive all cookies on this website. If you continue to use the website, we will also assume that you agree with our <a target="_blank" href="terms-of-use">Terms of Use</a>.'
 const buttonContinueTitle = segments.includes('bg') ? 'Приемам' : 'Accept';
 const buttonLearnmoreTitle = segments.includes('bg') ? 'Още информация' : 'Learn more';
 
